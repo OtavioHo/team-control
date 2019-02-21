73e022d81aa495a1804f862dbd1b45fa0ec6dc07
Otavio <otavio@aragoni.com>
2019-02-21 16:10:18 -0300

    auth middleware configuration

--- src/app/models/Manager.js
+++ src/app/models/Manager.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../config/auth')
 
 const Manager = new mongoose.Schema({
   email: {
@@ -15,5 +18,27 @@ const Manager = new mongoose.Schema({
     ref: 'Player'
   }
 })
+
+Manager.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 8)
+})
+
+Manager.methods = {
+  compareHash (password) {
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+Manager.statics = {
+  generateToken ({ id }) {
+    return jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.ttl
+    })
+  }
+}
 
 module.exports = mongoose.model('Manager', Manager)
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+
 const PlayerController = require('./app/controllers/PlayerController')
 const TeamController = require('./app/controllers/TeamController')
+const IncomeController = require('./app/controllers/IncomeController')
+const ManagerController = require('./app/controllers/ManagerController')
+const PaymentController = require('./app/controllers/PaymentController')
+const SessionController = require('./app/controllers/SessionController')
 
 routes.get('/', (req, res) => {
   return res.json({ teste: 'teste' })
@@ -14,5 +20,18 @@ routes.get('/players', PlayerController.all)
 
 routes.post('/teams', TeamController.store)
 routes.get('/teams', TeamController.all)
+
+routes.post('/incomes', IncomeController.store)
+routes.get('/incomes', IncomeController.all)
+
+routes.post('/managers', ManagerController.store)
+routes.get('/managers', ManagerController.all)
+
+routes.post('/payments', PaymentController.store)
+routes.post('/payments', PaymentController.all)
+
+routes.post('/session', SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
