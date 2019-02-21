b200cba13be55f99e8d8dc140b790f2e760a7dd9
Otavio <otavio@aragoni.com>
2019-02-21 14:31:00 -0300

    configured PlayerController and route

---
+++ src/app/controllers/PlayerController.js
@@ -0,0 +1,21 @@
+const Player = require('../models/Player')
+
+class PlayerController {
+  async store (req, res) {
+    const { email } = req.body
+
+    if (await Player.findOne({ email })) {
+      return res.status(400).json({ error: 'Player already exists' })
+    }
+
+    const user = await Player.create(req.body)
+
+    return res.json(user)
+  }
+
+  async players (req, res) {
+    return res.json(await Player.find())
+  }
+}
+
+module.exports = new PlayerController()
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const routes = express.Router()
 
+const PlayerController = require('./app/controllers/PlayerController')
+
 routes.get('/', (req, res) => {
   return res.json({ teste: 'teste' })
 })
+
+routes.post('/players', PlayerController.store)
+routes.get('/players', PlayerController.players)
 
 module.exports = routes
