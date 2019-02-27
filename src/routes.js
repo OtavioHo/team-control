d405c1ba0de84015a0e1a7d7df6e4de3617c1ff7
Otavio <otavio@aragoni.com>
2019-02-27 10:46:39 -0300

    Recatored routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,28 +8,43 @@ const authMiddleware = require('./app/middlewares/auth')
 
 routes.post('/login', controllers.LoginController.login)
 
-routes.post('/teams', controllers.TeamController.store)
-
-routes.get('/teams/:team_id/payments', controllers.PaymentController.payments)
+// Incomes
 routes.post('/teams/:team_id/incomes', controllers.IncomeController.store)
 routes.get('/teams/:team_id/incomes', controllers.IncomeController.incomes)
+routes.delete(
+  '/teams/:team_id/incomes/:income_id',
+  controllers.IncomeController.delete
+)
+routes.put(
+  '/teams/:team_id/incomes/:income_id',
+  controllers.IncomeController.update
+)
+
+// Payments
+routes.get('/teams/:team_id/payments', controllers.PaymentController.payments)
 routes.post('/teams/:team_id/payments', controllers.PaymentController.store)
+routes.delete(
+  '/teams/:team_id/payments/:payment_id',
+  controllers.PaymentController.delete
+)
+routes.put(
+  '/teams/:team_id/payments/:payment_id',
+  controllers.PaymentController.update
+)
+
+// Teams
+routes.post('/teams', controllers.TeamController.store)
 routes.get('/teams/:team_id/players', controllers.TeamController.players)
 routes.get(
   '/teams/:team_id/players/:user_id',
   controllers.TeamController.player
 )
 
-routes.delete(
-  '/teams/:team_id/incomes/:income_id',
-  controllers.IncomeController.delete
-)
-
+// Users
 routes.get('/users', controllers.PlayerController.show)
 routes.post('/users', controllers.PlayerController.store)
 routes.put('/users/:user_id', controllers.PlayerController.update)
 routes.delete('/users/:user_id', controllers.PlayerController.delete)
-
 routes.get('/users/teams', authMiddleware, controllers.TeamController.teams)
 
 module.exports = routes
